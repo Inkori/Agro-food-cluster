60c544b7505a5b44a61c556be00e3361531f2793
Roman Khocholava <khocholavaroman@gmail.com>
2018-04-16 16:47:07 +0300

    adaptive site without menu and animation

--- app/js/common.js
+++ app/js/common.js
@@ -14,9 +14,22 @@ $(function() {
 		owl.trigger('next.owl.carousel');
 	});
 	$(".prev").click(function(){
-		owl.trigger('next.owl.carousel');
+		owl.trigger('prev.owl.carousel');
 	});
 
 	$(".participants-item").equalHeights();
+	$("#news").equalHeights();
+
+	$(window).scroll(function() {
+		if ($(this).scrollTop() > $(this).height()) {
+			$('.top').addClass('active');
+		} else {
+			$('.top').removeClass('active');
+		}
+	});
+	$('.top').click(function() {
+		$('html, body').stop().animate({scrollTop: 0}, 'slow', 'swing');
+	});
+	
 
 });
